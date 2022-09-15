0c7addc4eac180b1f1fab8262cd7a14acc8d8225
Rhama Fahri Afriansyah <rhamafahri17@gmail.com>
2022-09-15 18:31:29 +0700

    RG-DP-02 done

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -22,4 +22,27 @@ describe("Test Cases -> Dashboard Page", () => {
   it("Found No Post for the First Time", () => {
     cy.contains("Found 0 photos");
   });
+
+  it("Contains Image URL and description input, and Publish Button", () => {
+    const image = cy.get("input[name='image']");
+    image
+      .should("be.visible")
+      .and("have.attr", "type", "url")
+      .and("have.attr", "required", "required")
+      .and("have.attr", "placeholder", "Image URL");
+
+    const description = cy.get("input[name='desc']");
+    description
+      .should("be.visible")
+      .and("have.attr", "type", "text")
+      .and("have.attr", "required", "required")
+      .and("have.attr", "placeholder", "What's on your mind?");
+
+    const button = cy.get("button[type='submit']");
+    button
+      .should("be.visible")
+      .contains("Publish!")
+      .should("have.css", "background-color", "rgb(79, 70, 229)")
+      .and("have.css", "color", "rgb(255, 255, 255)");
+  });
 });
